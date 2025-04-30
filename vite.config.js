d52c2e17d0c149f5beff8f547876ecd7f5bf0ae2
Connie Liu <cliu55567@gmail.com>
2025-04-30 12:22:42 -0700

    try 6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// This is correct ESM syntax
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // other config
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
