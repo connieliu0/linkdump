de5ddbd38eb38bff4478816440165d4de03cc381
Connie Liu <cliu55567@gmail.com>
2025-05-01 19:06:13 -0700

    easier testing

--- src/components/PasteArea.jsx
+++ src/components/PasteArea.jsx
@@ -590,7 +590,7 @@ const PasteArea = ({ onExport }) => {
     if (timeSettings) {
       inactivityTimer.current = setTimeout(() => {
         setIsInactive(true);
-      }, 180000); // 3 minutes
+      }, 2000); // 3 minutes
     }
   }, [timeSettings]);
 
