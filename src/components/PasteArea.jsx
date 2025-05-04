d0747f6a7660e6a6c154d0c90e042338b9a9d0bf
Connie Liu <cliu55567@gmail.com>
2025-05-04 01:08:47 -0700

    longer until it switches to screensaver

--- src/components/PasteArea.jsx
+++ src/components/PasteArea.jsx
@@ -588,7 +588,7 @@ const PasteArea = ({ onExport }) => {
     if (timeSettings) {
       inactivityTimer.current = setTimeout(() => {
         setIsInactive(true);
-      }, 1000); // 3 minutes
+      }, 180000); // 3 minutes in milliseconds
     }
   }, [timeSettings]);
 
