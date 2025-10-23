19f5d1604c12ca8745b9abb3f89e8a9ce82a270c
Connie Liu <cliu55567@gmail.com>
2025-10-23 11:26:57 -0400

    signup

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false
-  }
+    sourcemap: false,
+    assetsInclude: ['**/*.mp4', '**/*.webm', '**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif']
+  },
+  publicDir: 'public'
 })
